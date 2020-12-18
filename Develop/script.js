48b43a0ec5936388364c5b748bf9c12b2af4c948
Ariel Snow <arielbrizuelak@gmail.com>
2020-12-17 23:36:50 -0800

    Was able to add a prompt when you click the generate password

--- Develop/script.js
+++ Develop/script.js
@@ -14,8 +14,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// generator functions
 
+// generator functions
+function generatePassword(){
+  //this will create a prompt when generate password is selected.
+  enter = prompt("How long do you want your password characters to be? Please choose between 8 minimum to 128 maximum characters.")
+  if (!enter) {
+    alert("This needs a value please");
+  }
+  else if (enter < 8 || enter > 128){
+    enter = parseInt(prompt ("You need to choose between 8 and 128, please （；¬＿¬)"));
+  }
+  else {
+    confirmRandomLowerCase = confirm("Do you want LowerCase Letters?")
+    confirmRandomUpperCase = confirm("What about UpperCase Letters?")
+    confirmRandomSymbols = confirm("How about special symbols to spice it up?")
+    confirmRandomNumbers = confirm("You can't forget about numbers, a lot of new age passwords require at least one number, so how about it?")
+  }
+}
+
+//random generator functions for my Lower, upper, and Symbols.
 function getRandomLower() {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   return lowerCase[Math.floor(Math.random() * lowerCase.length  )];
@@ -34,4 +52,3 @@ function getRandomNumbers() {
   const Numbers= '1234567890';
   return Numbers[Math.floor(Math.random() * Numbers.length  )];
 }
-console.log(getRandomNumbers());
