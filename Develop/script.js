01497b185651b55dbaec7c65eeb2fef1d8ea73ae
Ariel Snow <arielbrizuelak@gmail.com>
2020-12-18 22:22:53 -0800

    finished the else if statements for 3 choices and started for the two

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var confirmRandomLowerCase;
+var confirmRandomUpperCase;
+var confirmRandomSymbols;
+var confirmRandomNumbers;
 
 // Write password to the #password input
 function writePassword() {
@@ -26,13 +29,49 @@ function generatePassword(){
     enter = parseInt(prompt ("You need to choose between 8 and 128, please （；¬＿¬)"));
   }
   else {
+    //contunues after you choose a number above 8 and lower than 128
     confirmRandomLowerCase = confirm("Do you want LowerCase Letters?")
     confirmRandomUpperCase = confirm("What about UpperCase Letters?")
     confirmRandomSymbols = confirm("How about special symbols to spice it up?")
     confirmRandomNumbers = confirm("You can't forget about numbers, a lot of new age passwords require at least one number, so how about it?")
   }
+  //This one is in case none of criterias were selected.
+  if (confirmRandomLowerCase && confirmRandomUpperCase && confirmRandomSymbols && confirmRandomnumbers){
+    choices = alert("You need to choose at least one of the choices!")
+  }
+  //This one is if all were selected
+  else if (confirmRandomLowerCase && confirmRandomUpperCase && confirmRandomSymbols && confirmRandomnumbers){
+    choices = contact(getRandomLower, getRandomUpperCase, getRandomSymbols, getRandomNumbers)
+  }
+  //if for 3 positive options
+  else if (confirmRandomLowerCase && ConfirmRandomUpperCase && ConfirmRandomSymbols)[
+    choices =  contact(getRandomLower, getRandomUpperCase, getRandomSymbols)
+  ]
+  else if (confirmLowerCase && confirmRandomUpperCase && confirmRandomNumbers){
+    choices = contact(getRandomLower, getRandomUpperCase, getRandomNumbers);
+  }
+  else if (confirmRandomUpperCase && confirmRandomSymbols && confirmRandomNumbers)[
+     choices = contact(getRandomUpperCase, getRandomSymbols, getRandomNumbers )
+  ]
+  else if (confirmLowerCase && confirmSymbols && confirmRandomNumbers)[
+    choices = contact(getRandomLower, getRandomSymbols, getRandomNumbers)
+  ]
+  // If 2 are positive
+  else if (confirmRandomLowerCase && confirmRandomUpperCase)[
+    choices = contact(getRandomLower, getRandomUpperCase)
+  ]
+  else if (confirmRandomLowerCase && confirmRandomNumbers)[
+    choices = contact(getRandomLower, getRandomNumbers)
+  ]
+  else if (confirmRandomLowerCase && confirmRandomSymbols)[
+    choices = contact(getRandomLower, getRandomSymbols)
+  ]
+  else if (UpperRandomUpperCase && confirmRandomNumbers)[
+    choices = contact(getRandomUpperCase, getRandomNumbers)
+  ]
 }
 
+var password = [];
 //random generator functions for my Lower, upper, and Symbols.
 function getRandomLower() {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
