93c6ddf03646ddb3a4f7b0224283e51d82fda379
Ariel Snow <arielbrizuelak@gmail.com>
2020-12-18 23:03:59 -0800

    broke something between line 22 and 27 but I have no idea what it is

--- Develop/script.js
+++ Develop/script.js
@@ -20,58 +20,100 @@ generateBtn.addEventListener("click", writePassword);
 
 // generator functions
 function generatePassword(){
+ 
   //this will create a prompt when generate password is selected.
   enter = prompt("How long do you want your password characters to be? Please choose between 8 minimum to 128 maximum characters.")
+
   if (!enter) {
     alert("This needs a value please");
   }
   else if (enter < 8 || enter > 128){
     enter = parseInt(prompt ("You need to choose between 8 and 128, please （；¬＿¬)"));
   }
-  else {
+ 
+    else {
     //contunues after you choose a number above 8 and lower than 128
     confirmRandomLowerCase = confirm("Do you want LowerCase Letters?")
     confirmRandomUpperCase = confirm("What about UpperCase Letters?")
     confirmRandomSymbols = confirm("How about special symbols to spice it up?")
     confirmRandomNumbers = confirm("You can't forget about numbers, a lot of new age passwords require at least one number, so how about it?")
-  }
+  };
   //This one is in case none of criterias were selected.
   if (confirmRandomLowerCase && confirmRandomUpperCase && confirmRandomSymbols && confirmRandomnumbers){
-    choices = alert("You need to choose at least one of the choices!")
+    choices = alert("You need to choose at least one of the choices!");
   }
   //This one is if all were selected
   else if (confirmRandomLowerCase && confirmRandomUpperCase && confirmRandomSymbols && confirmRandomnumbers){
-    choices = contact(getRandomLower, getRandomUpperCase, getRandomSymbols, getRandomNumbers)
+    choices = contact(getRandomLower, getRandomUpperCase, getRandomSymbols, getRandomNumbers);
   }
   //if for 3 positive options
-  else if (confirmRandomLowerCase && ConfirmRandomUpperCase && ConfirmRandomSymbols)[
+  else if (confirmRandomLowerCase && confirmRandomUpperCase && confirmRandomSymbols){
     choices =  contact(getRandomLower, getRandomUpperCase, getRandomSymbols)
-  ]
+  }
+     
   else if (confirmLowerCase && confirmRandomUpperCase && confirmRandomNumbers){
     choices = contact(getRandomLower, getRandomUpperCase, getRandomNumbers);
   }
-  else if (confirmRandomUpperCase && confirmRandomSymbols && confirmRandomNumbers)[
-     choices = contact(getRandomUpperCase, getRandomSymbols, getRandomNumbers )
-  ]
-  else if (confirmLowerCase && confirmSymbols && confirmRandomNumbers)[
+  else if (confirmRandomUpperCase && confirmRandomSymbols && confirmRandomNumbers){
+    choices = contact(getRandomUpperCase, getRandomSymbols, getRandomNumbers)
+  }
+     
+  else if (confirmLowerCase && confirmSymbols && confirmRandomNumbers){
     choices = contact(getRandomLower, getRandomSymbols, getRandomNumbers)
-  ]
+  }
+    
   // If 2 are positive
-  else if (confirmRandomLowerCase && confirmRandomUpperCase)[
+  else if (confirmRandomLowerCase && confirmRandomUpperCase){
     choices = contact(getRandomLower, getRandomUpperCase)
-  ]
-  else if (confirmRandomLowerCase && confirmRandomNumbers)[
-    choices = contact(getRandomLower, getRandomNumbers)
-  ]
-  else if (confirmRandomLowerCase && confirmRandomSymbols)[
-    choices = contact(getRandomLower, getRandomSymbols)
-  ]
-  else if (UpperRandomUpperCase && confirmRandomNumbers)[
-    choices = contact(getRandomUpperCase, getRandomNumbers)
-  ]
-}
+  }
+    
+  else if (confirmRandomLowerCase && confirmRandomNumbers){
+  choices = contact(getRandomLower, getRandomNumbers)
+  }  
 
-var password = [];
+  else if (confirmRandomLowerCase && confirmRandomSymbols){
+    choices = contact(getRandomLower, getRandomSymbols)
+  }
+   
+  
+  else if (confirmRandomUpperCase && confirmRandomNumbers){
+    choices = contact(getRandomUpperCase, getRandomNumbers)
+  }
+    
+  
+  else if (ConfirmRandomUpperCase && confirmRandomSymbols){
+    choices = contact(getRandomUpperCase, getRandomSymbols)
+  }
+    
+  
+  else if (confirmRandomSymbols && confirmRandomNumbers){
+    choices = contact(getRandomSymbols, getRandomNumbers)
+  }
+  
+  // For 1 choices
+  else if (confirmLowerCase){
+    choices = contact(getRandomLower)
+  }
+  
+  
+  else if (confirmRandomUpperCase){
+    choices = contact(getRandomUpperCase)
+  }
+   
+  else if (confirmRandomSymbols){
+    choices = contact(getRandomSymbols)
+  }
+    
+
+  else if (confirmRandomNumbers){
+    choices = contact(GetRandomNumbers)
+  }
+    
+  
+  var password = [];
+
+ 
+
 //random generator functions for my Lower, upper, and Symbols.
 function getRandomLower() {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
